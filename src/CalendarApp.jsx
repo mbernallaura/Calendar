93477dac20b697d8ec366ab90093191852711a90
laura <mbernallaura@gmail.com>
2024-10-18 18:50:56 -0500

    Realizar el esquema de la app Realizar la proteccion de rutas

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,8 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRouter } from "./router";
 
 export const CalendarApp = () => {
     return (
-        <div>
-            calendar
-        </div>
+        <BrowserRouter>
+            <AppRouter/>
+        </BrowserRouter>
     )
 }
